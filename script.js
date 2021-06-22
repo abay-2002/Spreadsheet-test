b6db06ce78500f7354f4a56d67f6418106e5cb59
abay-2002 <akbar121202@gmail.com>
2021-06-22 21:30:43 +0700

    bahan belajar

---
+++ script.js
@@ -0,0 +1,16 @@
+// multidimensional array
+let mahasiswa = [
+	["akbar","32220036"],
+	["abay","32220035"]
+];
+console.log(mahasiswa[0][0]);
+
+// array associative
+let ascArr = {"nama": "akbar angkasa", "nim": "32220036"};
+console.log(ascArr["nama"]);
+
+// forEach
+// The forEach() method calls a function once for each element in an array, in order.
+
+// forEach PHP
+// pengulangan untuk array
